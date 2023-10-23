80eb3f2cd4b0f0f3c62a29421329746bf02bead3
tiger <44923556+eeeeaa@users.noreply.github.com>
2023-10-23 20:42:55 +0700

    followed loader guide

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import _ from 'lodash';
+import myName from './myName';
+import './style.css'; //import resources so webpack can see it
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
 
-function component(){
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
+function component() {
     const element = document.createElement('div');
+    element.classList.add('hello');
 
     // Lodash is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ') + ',' + myName('Cody');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,58 @@
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
+//NOTE: json is natively supported
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: { //chains of loaders, it runs in order from top to bottom
+        rules: [
+            {
+                test: /\.css$/i, //load and optimize css
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i, //load and optimize image
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i, //load and optimize fonts
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i, //load and optimize csv
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i, //load and optimize xml
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.toml$/i, //load and optimize toml
+                type: 'json',
+                parser: {
+                    parse: toml.parse,
+                },
+            },
+            {
+                test: /\.yaml$/i, //load and optimize yaml
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                },
+            },
+            {
+                test: /\.json5$/i, //load and optimize json5
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+        ],
     },
 };
